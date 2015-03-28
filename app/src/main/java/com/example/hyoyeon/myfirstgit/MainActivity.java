18130ef79693bf346c40dd706dd5f2f3a44b4846
hyok <gbdus11@gmail.com>
2015-03-28 17:52:22 +0900

    go

--- app/src/main/java/com/example/hyoyeon/myfirstgit/MainActivity.java
+++ app/src/main/java/com/example/hyoyeon/myfirstgit/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
 
 
@@ -22,6 +21,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+        
     }
 
     @Override
