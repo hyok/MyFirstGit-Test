80d62337b8c6212ae1ee69da6f999e28f3663fb1
Francis Kim <kims_0121@naver.com>
2015-03-28 18:04:49 +0900

    Merge Test

--- app/src/main/java/com/example/hyoyeon/myfirstgit/MainActivity.java
+++ app/src/main/java/com/example/hyoyeon/myfirstgit/MainActivity.java
@@ -13,8 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        Integer twb = 0;
     }
 
 
